24116fd50592ccef537b75c41e820fcc35438c2f
Dhanush046 <dhanush.69040@gmail.com>
2025-06-30 01:36:40 +0530

    vercel

--- src/App.js
+++ src/App.js
@@ -19,73 +19,33 @@
 
 // export default App;
 
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./components/Home";
-// import About from "./components/About";
-// import NavBar from "./components/NavBar";
-// import { OurProducts } from "./components/OurProducts";
-// // import ProductAbout from "./components/ProductAbout";
-// import ImageSlider from "./components/ImageSlider";
-// import AdditionalSegment from "./components/AdditionalSegment";
-// import Additional from "./components/Additional";
-// import ProductDetail from "./components/ProductDetail"; // ✅ Make sure this is imported
-
-// function App() {
-//   return (
-//     <Router>
-//       <NavBar />
-//       <Routes>
-//         <Route path="/" element={
-//           <>
-//             <Home />
-//             <About />
-//             <ImageSlider />
-//             <OurProducts />
-//             <AdditionalSegment />
-//             <Additional />
-//           </>
-//         } />
-//         <Route path="/product/:id" element={<ProductDetail />} /> {/* ✅ Dynamic Route */}
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import { OurProducts } from "./components/OurProducts";
+// import ProductAbout from "./components/ProductAbout";
 import ImageSlider from "./components/ImageSlider";
 import AdditionalSegment from "./components/AdditionalSegment";
 import Additional from "./components/Additional";
-// import ProductDetail from "./components/ProductDetail";
-
-// 🔧 Replace this with your actual GitHub repo name
-const BASENAME = "/clarkscomponents";
+import ProductDetail from "./components/ProductDetail"; // ✅ Make sure this is imported
 
 function App() {
   return (
-    <Router basename={BASENAME}>
+    <Router>
       <NavBar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Home />
-              <About />
-              <ImageSlider />
-              <OurProducts />
-              <AdditionalSegment />
-              <Additional />
-            </>
-          }
-        />
-        {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
+        <Route path="/" element={
+          <>
+            <Home />
+            <About />
+            <ImageSlider />
+            <OurProducts />
+            <AdditionalSegment />
+            <Additional />
+          </>
+        } />
+        <Route path="/product/:id" element={<ProductDetail />} /> {/* ✅ Dynamic Route */}
       </Routes>
     </Router>
   );
