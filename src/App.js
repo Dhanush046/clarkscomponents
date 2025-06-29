61b57b216d7ccc9b67c9761df885a553836efa4c
Dhanush046 <dhanush.69040@gmail.com>
2025-06-30 01:27:27 +0530

    fix: add basename for GitHub Pages and other improvements

--- src/App.js
+++ src/App.js
@@ -19,33 +19,73 @@
 
 // export default App;
 
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import NavBar from "./components/NavBar";
+// import { OurProducts } from "./components/OurProducts";
+// // import ProductAbout from "./components/ProductAbout";
+// import ImageSlider from "./components/ImageSlider";
+// import AdditionalSegment from "./components/AdditionalSegment";
+// import Additional from "./components/Additional";
+// import ProductDetail from "./components/ProductDetail"; // ✅ Make sure this is imported
+
+// function App() {
+//   return (
+//     <Router>
+//       <NavBar />
+//       <Routes>
+//         <Route path="/" element={
+//           <>
+//             <Home />
+//             <About />
+//             <ImageSlider />
+//             <OurProducts />
+//             <AdditionalSegment />
+//             <Additional />
+//           </>
+//         } />
+//         <Route path="/product/:id" element={<ProductDetail />} /> {/* ✅ Dynamic Route */}
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import { OurProducts } from "./components/OurProducts";
-// import ProductAbout from "./components/ProductAbout";
 import ImageSlider from "./components/ImageSlider";
 import AdditionalSegment from "./components/AdditionalSegment";
 import Additional from "./components/Additional";
-import ProductDetail from "./components/ProductDetail"; // ✅ Make sure this is imported
+// import ProductDetail from "./components/ProductDetail";
+
+// 🔧 Replace this with your actual GitHub repo name
+const BASENAME = "/clarkscomponents";
 
 function App() {
   return (
-    <Router>
+    <Router basename={BASENAME}>
       <NavBar />
       <Routes>
-        <Route path="/" element={
-          <>
-            <Home />
-            <About />
-            <ImageSlider />
-            <OurProducts />
-            <AdditionalSegment />
-            <Additional />
-          </>
-        } />
-        <Route path="/product/:id" element={<ProductDetail />} /> {/* ✅ Dynamic Route */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+              <About />
+              <ImageSlider />
+              <OurProducts />
+              <AdditionalSegment />
+              <Additional />
+            </>
+          }
+        />
+        {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
       </Routes>
     </Router>
   );
